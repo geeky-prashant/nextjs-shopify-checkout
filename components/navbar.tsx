53ae5cee9e287fbeaad438aba2a46a747cd000c5
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-02-14 00:17:43 +0530

    Completed navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,18 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="bg-[#fff] sticky top-0 z-30 border-b">
+      <div
+        className="flex items-center justify-center mx-auto max-w-[1280px] px-6 lg:px-20 relative py-6"
+      >
+        {/* Logo  */}
+        <Link href="/">
+          <Image src="/logo.svg" alt="logo" width={120} height={45} priority />
+        </Link>
+      </div>
+    </nav>
   )
 }
 
