8a820a84360f79d3aebadf688b250eed7ab1128e
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-02-14 01:55:53 +0530

    Completed Payment Link section

--- app/(checkout)/page.tsx
+++ app/(checkout)/page.tsx
@@ -1,7 +1,58 @@
+import Link from "next/link";
+import Image from "next/image";
+import { FOOTER_LINKS, PAYMENT_LINKS } from "@/constants";
+
 const Checkout = () => {
   return (
-    <div>Checkout</div>
-  )
-}
+    <div className="bg-white">
+      <div className="flex items-start flex-col md:flex-row">
+        {/* Left Col  */}
+        <div className="bg-white w-1/2 h-screen flex justify-end">
+          <div className="flex justify-between flex-col max-w-[1280px] w-[80%]">
+            {/* Top Content  */}
+            <div className="w-full py-6 px-8">
+              {/* Express Checkout  */}
+              <div className="flex items-center justify-center flex-col gap-3">
+                <h1 className="text-[#9D9D9D] text-sm">Express Checkout</h1
+                >
+                <div className="grid grid-cols-3 gap-2 w-full">
+                  {
+                    PAYMENT_LINKS.map((data) => (
+                      <div
+                        key={data.key}
+                        style={{ backgroundColor: data.bgColor }}
+                        className="rounded-sm flex items-center justify-center cursor-pointer"
+                      >
+                        <Image src={data.src} alt="logo" width={60} height={10} priority />
+                      </div>
+                    ))
+                  }
+                </div>
+              </div>
+            </div>
 
-export default Checkout
+            {/* Bottom Footer  */}
+            <div className="border-t w-full">
+              <div className="flex items-center gap-4 py-6 px-0 text-[#2683C2]">
+                {FOOTER_LINKS.map((data) => (
+                  <Link href={data.href} key={data.key} className="underline">
+                    {data.title}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Right Col  */}
+        <div className="bg-[#F5F5F5] w-1/2 h-screen">
+          <div className="flex items-start justify-start max-w-[1280px] py-6 px-8 w-[60%]">
+            Right
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
---
+++ constants/index.ts
@@ -0,0 +1,37 @@
+// FOOTER_LINKS
+export const FOOTER_LINKS = [
+  {
+    key: 1,
+    href: "",
+    title: "Refund policy",
+  },
+  {
+    key: 2,
+    href: "",
+    title: "Shipping policy",
+  },
+  {
+    key: 3,
+    href: "",
+    title: "Terms of service",
+  },
+];
+
+// PAYMENT_LINKS
+export const PAYMENT_LINKS = [
+  {
+    key: 1,
+    src: "/shoppay_logo.png",
+    bgColor: "#beb3e8",
+  },
+  {
+    key: 2,
+    src: "/paypal_logo.png",
+    bgColor: "#FFC635",
+  },
+  {
+    key: 3,
+    src: "/gpay_logo.webp",
+    bgColor: "#0b0b0b",
+  },
+];
