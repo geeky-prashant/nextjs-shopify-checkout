1b10e8dba637e193ac34d8dfce127deea62f3b95
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-02-15 01:33:22 +0530

    Installed react icons pacakage

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="bg-[#fff] sticky top-0 z-30 border-b">
       <div
-        className="flex items-center justify-center mx-auto max-w-[1280px] px-6 lg:px-20 relative py-6"
+        className="relative flex items-center justify-center mx-auto max-w-[1280px] px-6 lg:px-20 py-6"
       >
         {/* Logo  */}
         <Link href="/">
