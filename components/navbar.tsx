ee4036ac93e20ea43a4e86e12d989d9a827db063
Prashant Bhardwaj <geekyprashant840@gmail.com>
2024-02-15 01:41:54 +0530

    Added cart in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,23 @@
-import Image from "next/image"
 import Link from "next/link"
+import Image from "next/image"
+import { HiOutlineShoppingBag } from "react-icons/hi2";
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#fff] sticky top-0 z-30 border-b">
+    <nav className="bg-[#fff] sticky top-0 z-30 border-b w-full">
       <div
-        className="relative flex items-center justify-center mx-auto max-w-[1280px] px-6 lg:px-20 py-6"
+        className="relative flex items-center justify-center mx-auto max-w-[1040px] px-6 lg:px-20 py-6 w-full"
       >
         {/* Logo  */}
         <Link href="/">
           <Image src="/logo.svg" alt="logo" width={120} height={45} priority />
+        </Link>
+
+        {/* Cart  */}
+        <Link href="/" className="absolute left-0">
+          <HiOutlineShoppingBag
+            className="text-[#2683C2] text-xl"
+          />
         </Link>
       </div>
     </nav>
